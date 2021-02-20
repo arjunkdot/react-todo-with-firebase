fe6147600949f2488cd99a8bfe96a76d7f8dd680
Arjun K <arjunkdot@gmail.com>
2021-02-20 23:47:59 +0530

    Readme updated

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,7 +1,12 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  //Put your keys here
+  apiKey: "AIzaSyAPBcKRSJzg0EgTe2LFc-hg3_bZedo_otg",
+  authDomain: "todo-react-app-146cb.firebaseapp.com",
+  projectId: "todo-react-app-146cb",
+  storageBucket: "todo-react-app-146cb.appspot.com",
+  messagingSenderId: "1042044268488",
+  appId: "1:1042044268488:web:a4ce3981b6e6a8ee005afd",
 };
 
 firebase.initializeApp(firebaseConfig);
